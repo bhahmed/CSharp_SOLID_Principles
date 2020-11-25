00c653c7ae2b881bfb7004c595bfe8585b35de6a
AhmedBenHmida <ahmed.benhmida@newaccess.ch>
2020-11-25 19:59:40 +0100

    Apply LSP

--- LSPLibrary/Employee.cs
+++ LSPLibrary/Employee.cs
@@ -6,23 +6,11 @@ using System.Threading.Tasks;
 
 namespace LSPLibrary
 {
-    public class Employee
+    public class Employee : BaseEmployee, IManaged
     {
-        public string FirstName { get; set; }
+        public IEmployee Manager { get; set; }
 
-        public string LastName { get; set; }
-
-        public Employee Manager { get; set; }
-
-        public object Salary { get; set; }
-
-        public virtual void CalculatePerHourRate(int rank)
-        {
-            decimal baseAmount = 12.50M;
-            Salary = baseAmount + (rank * 2);
-        }
-
-        public virtual void AssignManager(Employee manager)
+        public void AssignManager(IEmployee manager)
         {
             Manager = manager;
         }
