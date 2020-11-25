2649880b0d549e3760dc46fef823cb7d77c59fe1
AhmedBenHmida <ahmed.benhmida@newaccess.ch>
2020-11-25 19:24:04 +0100

    LSP initial code

---
+++ LSPLibrary/Employee.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSPLibrary
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Employee Manager { get; set; }
+
+        public object Salary { get; set; }
+
+        public virtual void CalculatePerHourRate(int rank)
+        {
+            decimal baseAmount = 12.50M;
+            Salary = baseAmount + (rank * 2);
+        }
+
+        public virtual void AssignManager(Employee manager)
+        {
+            Manager = manager;
+        }
+    }
+}
